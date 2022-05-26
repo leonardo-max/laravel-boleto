6190b4922ec2687f27c18aba609cc9e109c953aa
Leonardo Max <l3onardomax@hotmail.com>
2022-05-26 16:16:52 -0300

    fixed
    fixed

--- src/Cnab/Retorno/Cnab400/Banco/Itau.php
+++ src/Cnab/Retorno/Cnab400/Banco/Itau.php
@@ -231,7 +231,7 @@ class Itau extends AbstractRetorno implements RetornoCnab400
             ->setValorDesconto(Util::nFloat($this->rem(241, 253, $detalhe) / 100, 2, false))
             ->setValorRecebido(Util::nFloat($this->rem(254, 266, $detalhe) / 100, 2, false))
             ->setValorMora(Util::nFloat($this->rem(267, 279, $detalhe) / 100, 2, false))
-            ->setValorMulta(Util::nFloat($this->rem(280, 292, $detalhe) / 100, 2, false));
+            ->setValorMulta(Util::nFloat($this->rem(280, 292, $detalhe) / 100, 2, false))
             ->setNomePagador($this->rem(325, 354, $detalhe));
 
         $msgAdicional = str_split(sprintf('%08s', $this->rem(378, 385, $detalhe)), 2) + array_fill(0, 4, '');
